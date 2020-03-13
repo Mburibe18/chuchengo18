a9eb84763a87af52cbe8cf0afc6479f37e15bc2b
Mburibe18 <mburibe9854@gmail.com>
2020-03-13 11:03:11 -0500

    cambios 2.0

--- clases/clase #6/tarea calculadora.py
+++ clases/clase #6/tarea calculadora.py
@@ -10,7 +10,11 @@ def dividir (x,y):
     division = x/y
     return division
 
+def multiplicar (x,y):
+    multiplicacion = x*y
+    return multiplicacion
+
 print (sumar(3,8))
 print (restar(11,7))
 print (dividir (500,20))
-    
+print (multiplicar (5,30))  
