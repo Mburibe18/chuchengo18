5bb4a91881f641c833b0058742a5866442e3c985
Mburibe18 <mburibe9854@gmail.com>
2020-03-27 10:43:27 -0500

    new changes

--- clases/clase_cuarentena.py/menus.py
+++ clases/clase_cuarentena.py/menus.py
@@ -3,11 +3,11 @@ MENSAJE_BIENVENIDO = "hola , khe onda"
 #--------CODIGO--------
 PREGUNTA_EDAD = "Dime tu age"
 #--------CODIGO--------
-PREGUNTA_LISTA_DE_COMPRAS = "deseas adquirir articulos ? s ->si n ->no "
+PREGUNTA_LISTA_DE_COMPRAS = "do you like to adquirir articulos ? s ->si n ->no "
 #--------CODIGO--------
-listaCompras = "this will be tu lista de compras producto a producto"
+listaCompras = "this will be tu lista de compras product by product"
 #--------CODIGO--------
-eliminarProducto = "do u like to delete  un producto de la lista ? cual ? s ->si n->no"
+eliminarProducto = "do u like to delete  un product de la lista ? cual ? s ->si n->no"
 #--------CODIGO--------
 PREGUNTA_NUMERO = "ingresa el number de products que quieras eliminar"
 #---------------------------------
@@ -17,30 +17,30 @@ _desicion = int (input("""
     ingrese :
     1-age
     2-lista de productos to buy
-    3-lista de compras producto a producto
-    4-delete un producto de la lista de compras
+    3-lista de compras product by product
+    4-delete un product de la lista de compras
     5-salir
 """))
 
 while (_desicion != 5):
     if(_desicion ==1):
-        _edadUsuario = int(input(PREGUNTA_EDAD))
-        if  ((_edadUsuario >= 0) and (_edadUsuario <= 17)) :
+        _ageUsuario = int(input(PREGUNTA_EDAD))
+        if  ((_ageUsuario >= 0) and (_ageUsuario <= 17)) :
             print("income denied")
-        elif((_edadUsuario >= 18) and (_edadUsuario <= 29)):
+        elif((_ageUsuario >= 18) and (_ageUsuario <= 29)):
              print("adelante , pase pss")
-        elif (_edadUsuario >=30) and (_edadUsuario <=59):
-            print("obtienes un discount del 30%")
+        elif (_ageUsuario >=30) and (_ageUsuario <=59):
+            print("wow ,you have un discount del 30%")
         else:
-            print("obtienes un discount igualito a tu edad")
+            print("wow,you have un discount igualito a tu edad")
     elif (_desicion ==2) :
-        productos = []
+        products = []
         _listaCompras = input(PREGUNTA_LISTA_DE_COMPRAS)
         #TE FALTABA DOBLE IGUAL QUE SE EMPLEA PARA PREGUNTAR
         while (_listaCompras =="s") :
-            productos.append (input("ingrese :"))
+            products.append (input("ingrese :"))
             _listaCompras = input (PREGUNTA_LISTA_DE_COMPRAS)
-        print (productos)
+        print (products)
 
 
     else:
@@ -49,8 +49,8 @@ while (_desicion != 5):
       ingrese :
       1-age
       2-lista de productos to buy
-    3-lista de compras producto a producto
-    4-delete un producto de la lista de compras
+    3-lista de compras product by product
+    4-delete un product de la lista de compras
     5-salir
 """))
 print("gracias por la confianza wey")
