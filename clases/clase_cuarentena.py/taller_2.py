d59f8241f7c5a654f11e161b8a4dd7fd860f12a7
Mburibe18 <mburibe9854@gmail.com>
2020-03-28 15:34:33 -0500

    taller Hospital

--- clases/clase_cuarentena.py/taller_2.py
+++ clases/clase_cuarentena.py/taller_2.py
@@ -1,11 +1,72 @@
-def mostrar_dos_listas(lista_1,list_2):
-    contador =1
-    for elemento in lista:
-        print(contador,"-",elemento)
+
+MENSAJE_OF_ENTRANCE ="Hi , welcome to this hospital"
+
+print (MENSAJE_OF_ENTRANCE)
+
+
+_decition = int (input("""
+    enter the number :
+    1- to know the list of our doctors , nurses and patients
+    2- to see patients status
+    3- to create a new list of persons
+    4- log out
+"""))
+while (_decition != 4):
+    if (_decition == 1):
+        def show_three_lists ( list_number_1 , list_number_2 , list_number_3):
+            if ( len ( list_number_1 ) ==  len ( list_number_2) == len (list_number_3)):
+                print ("doctors -" , "nurses -" , "patients")
+                for i  in range ( len ( list_number_1 )):
+                    print ( list_number_1 [ i ] , list_number_2 [ i ] , list_number_3 [i])
+
+            
+    
+
+        doctors = ["Mafe --", "Elena --", "Ysabella --", "The teacher --", "Marco --"]
+        nurses = ["Santiago --", "Valeria --", "Santiago H --", "Daniel Zarate --", "Camila Herrera M --"]
+        patients = ["Camila Herrera B", "  Juanes", "Ines", "Benito", "Dubeimar"]   
+
+        show_three_lists (doctors , nurses , patients)
         
+    elif (_decition == 2):
+        def show_two_lists ( list_number_1 , list_number_2):
+            if ( len ( list_number_1 ) ==  len ( list_number_2 )):
+                print ("paatients -" , "status")
+                for i  in range ( len ( list_number_1 )):
+                    print ( list_number_1 [ i ] , list_number_2 [ i ])
+
+        patients = ["Jimena --", "Mateo --", "Ricardo --", "Antonia --", "Fredy --"] 
+        status = ["UCI", "SERIOUS", "SERIOUS", "STABLE", "UCI"]
+
+        show_two_lists (patients, status)
 
 
+    elif (_decition == 3):
+        def completelist ():
+            list = []
+            decition = input ("enter y--> to add more persons n--> for no to add more people : ")
+            while (decition == "y"):
+                list.append (input("enter a person in the list : "))
+                decition = input ("enter y--> to add more people n--> for no to add more people : ")
+            return list 
+        print ("enter the doctors list : ")
+        doctors = completelist ()
+        print ("enter nurses list : ")
+        nurses = completelist ()
+        print ("enter the patients list : ")
+        patients = completelist ()
+        
+        show_three_lists (doctors, nurses, patients)
 
-    print("doctores","enfermer@s")
-    for in range (len(lista_1))
-        print(lista_1[1], lista_2[1])
+    
+    else:
+        print("enter a valid value")
+
+    _decision = int (input("""
+      ingrese :
+        1- to know the list of our doctors , nurses and patients
+        2- to see patients status
+        3- to create a new list of persons
+        4- log out
+        """))
+print("thanks for using the program bro")
